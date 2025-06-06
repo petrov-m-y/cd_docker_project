74006e1716dae7c83c22c1f07c1403aef5d670d1
Mikhail Petrov <petrov.m.y@edu.mirea.ru>
2025-06-06 19:54:30 +0300

    Second commit for test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'Hello, Continuous Deployment!'
+    return 'Hello, Continuous Deployment! Second commit!'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
